f5b626406251df04ad817eef59ac3bc217a93e73
Frank Rose <frankrosework@gmail.com>
2018-04-10 00:21:05 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -9,12 +9,12 @@ function lastImageSelector () {
 
 function ninjaBabySelector () {
   return $('#baby-ninja');
-};
+}
 
 function divSelector () {
   return $('.pics');
-};
+}
 
 function firstListItem () {
     return $('ul #pic-list');
-};
+}
